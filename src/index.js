f0147e7659f2d1418bb7e4209fc26744405523b3
mariaanisimova3091 <mariaanisimova3091@gmail.com>
2021-08-23 23:07:19 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+exports.min = function min(array) {
+    if (!arguments[0] || !array.length) return 0;
+    return array.sort(function (a, b) {
+        return a - b;
+    })[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!arguments[0] || !array.length) return 0;
+    return array.sort(function (a, b) {
+        return b - a;
+    })[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!arguments[0] || !array.length) return 0;
+    const sum = array.reduce((a, b) => a + b);
+    return sum / array.length;
+};
